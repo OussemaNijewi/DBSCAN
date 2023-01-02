9166a1b79152bc022cdab0950db5a47b161fb7cd
Oussema <oussemasn@gmail.com>
2023-01-02 18:30:49 -0500

    nearest neighbor class for grouping points

--- NearestNeighbors.cpp
+++ NearestNeighbors.cpp
@@ -1,3 +1,21 @@
-class NearestNeighbors {
+#include <list>
 
+class NearestNeighbors {
+    list<Point3D> points;
+
+    public:
+
+        NearestNeighbors(list<Point3D> listOfPoints){
+            points = listOfPoints;
+        }
+
+        list<Point3D> rangeQuery(Point3D p, double eps){
+            list<Point3D> newList;
+            for(Point3D const &point : p){
+                if (p.distance(point)<=eps){
+                    newList.push_back(point);
+                }
+            }
+            return newList;
+        }
 };
